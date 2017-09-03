5ce5132af76b630820e97f88482e368effc2505c
paulan <paulan94@gmail.com>
2017-09-02 20:45:33 -0700

    learning command line interfaces with argparse lib

---
+++ string_cat_formatting.py
@@ -0,0 +1,20 @@
+names = ["jeff", "gary", "jill", "samantha"]
+
+##import os
+##
+##location_of_files = "/Users/paul/Documents/Intermediate-Python-Tutorials/"
+##
+##file_name = 'string_cat_formatting.py'
+##
+##with open(os.path.join(location_of_files, file_name)) as f:
+##    print(f.read())
+
+who = 'Gary'
+how_many = 12
+
+sentence = {}
+print (who, 'bought', how_many, 'apples today!')
+
+print('{} bought {} apples today!'.format(who, how_many))
+
+
